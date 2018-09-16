92912dfb185808795125e5ddd078f5de5f7ae304
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-09-16 23:01:57 +0200

    guest and user guards

---
+++ src/guards/user/user.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if (this.authService.isLoggedIn()) {
+        return true;
+
+      } else {
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+      }
+  }
+}
