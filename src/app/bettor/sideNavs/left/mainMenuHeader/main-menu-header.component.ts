95a2138d914008998e3526ca33558e317b589458
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-10-08 17:03:56 +0200

    allow you to join a public contest

--- src/app/bettor/sideNavs/left/mainMenuHeader/main-menu-header.component.ts
+++ src/app/bettor/sideNavs/left/mainMenuHeader/main-menu-header.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../../../../services/auth/auth.service';
-import { LoadingController, MenuController } from '@ionic/angular';
+import { LoadingController, MenuController, ActionSheetController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { UserService } from '../../../../../services/user/user.service';
 import { takeUntil } from 'rxjs/operators';
 import { UserModel } from '../../../../../models/user.model';
 import { Subject } from 'rxjs';
+import { ContestService } from '../../../../../services/bettor/contest.service';
 
 @Component({
   selector: 'main-menu-header',
@@ -16,6 +17,7 @@ export class MainMenuHeaderComponent implements OnInit, OnDestroy {
 
   searchText: string;
   user: UserModel;
+  contestActionSheetButtons = [];
   private unsubscribeAll: Subject<any>;
 
   constructor(
@@ -23,7 +25,10 @@ export class MainMenuHeaderComponent implements OnInit, OnDestroy {
     private loadingCtrl: LoadingController,
     private router: Router,
     private menuCtrl: MenuController,
-    private userService: UserService
+    private userService: UserService,
+    private actionSheetCtrl: ActionSheetController,
+    private contestService: ContestService,
+    private toastCtrl: ToastController,
   ) {
     this.unsubscribeAll = new Subject();
   }
@@ -36,6 +41,21 @@ export class MainMenuHeaderComponent implements OnInit, OnDestroy {
           this.user = user;
         }
       });
+
+    this.contestService.getContest('PUBLIC').subscribe(publicContests => {
+      for (const contest in publicContests) {
+        if (publicContests.hasOwnProperty(contest)) {
+          const contestObj = {
+            text: publicContests[contest].caption,
+            handler: () => {
+              this.joinContest(publicContests[contest].id, publicContests[contest].caption);
+            }
+          };
+
+          this.contestActionSheetButtons.push(contestObj);
+        }
+      }
+    });
   }
 
   async presentLoading() {
@@ -46,6 +66,28 @@ export class MainMenuHeaderComponent implements OnInit, OnDestroy {
     return await loading.present();
   }
 
+  async presentContestActionSheet() {
+    this.menuCtrl.toggle('main');
+    const contestActionSheet = await this.actionSheetCtrl.create({
+      header: 'Concours Public',
+      subHeader: 'Selectionner un concours public pour vous inscrire',
+      buttons: this.contestActionSheetButtons,
+    });
+
+    await contestActionSheet.present();
+  }
+
+  async presentToast(message: string, toastType: string = 'success') {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'bottom',
+      cssClass: toastType === 'success' ? 'toastSuccess' : 'toastError',
+      mode: 'ios',
+    });
+    toast.present();
+  }
+
   logOut() {
     this.authService.logout();
     this.presentLoading().then(() => {
@@ -54,7 +96,7 @@ export class MainMenuHeaderComponent implements OnInit, OnDestroy {
   }
 
   openProfil() {
-    this.menuCtrl.close('main').then(() => {
+    this.menuCtrl.toggle('main').then(() => {
       this.menuCtrl.enable(false, 'main');
       this.menuCtrl.enable(true, 'profil');
       this.menuCtrl.toggle('profil');
@@ -64,6 +106,14 @@ export class MainMenuHeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.unsubscribeAll.next();
     this.unsubscribeAll.complete();
+  }
+
+  joinContest(contestId: string, contestName: string) {
+    this.userService.subscribeToContest(contestId).then(resp => {
+      this.presentToast(`Vous avez rejoins le concours ${contestName}`);
+    }).catch(error => {
+      this.presentToast('Une erreur s\'est produite', 'error');
+    } );
   }
 
 }
